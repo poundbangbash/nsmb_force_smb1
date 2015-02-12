1245359c72a77df56f2f4a8efc4a778a9a2a8e50
eholtam <eholtam@mac.com>
2015-02-11 20:59:21 -0600

    Fixed if statement formatting

--- nsmb_config_force_smb1.sh
+++ nsmb_config_force_smb1.sh
@@ -5,20 +5,20 @@ nsmb_file=/etc/nsmb.conf
 if [ -f $nsmb_file ]
 then
     #if smb2 is forced, exit.
-    if [ `/usr/bin/grep -wq "smb_neg=smb2_only" $nsmb_file` ]
+    if /usr/bin/grep -wq "smb_neg=smb2_only" $nsmb_file
     then
         #SMB2 protocol already forced.  smb_neg=smb2_only found
         exit 0
     fi
     
     #if nsmb.conf file exists then check if smb1 is forced already
-    if [ `/usr/bin/grep -wq "smb_neg=smb1_only" $nsmb_file` ]
+    if /usr/bin/grep -wq "smb_neg=smb1_only" $nsmb_file
     then
         #smb_neg=smb1_only already exists.  Exit.
         exit 0
     else
         #if not then check if the default header exists
-        if [ `/usr/bin/grep -wq "\[default]" $nsmb_file` ]
+        if /usr/bin/grep -wq "\[default]" $nsmb_file
         then
             #if [default] exists use /usr/bin/sed to find and replace it with a new line under it for SMB1
             /usr/bin/sed -i -e 's/\[default\]/\[default\]\'$'\nsmb_neg=smb1_only/' $nsmb_file                    
